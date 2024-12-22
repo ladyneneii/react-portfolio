5ab6803afe2cb16fed1140ca2da994afc2aa2191
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2024-12-22 21:26:22 +0800

    feat(box): make header sticky if unfolded

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -1,7 +1,7 @@
 import { UserPrefContext } from "@/context/UserPrefContext";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { getConditionalSmoothTransition } from "@/shared";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 
 type Props = {
@@ -26,10 +26,32 @@ const Box = ({
   const [unfold, setUnfold] = useState(true);
 
   const foldCondition = !isFoldable || (isFoldable && unfold);
+  const headerRef = useRef<HTMLDivElement | null>(null);
+  const [isSticky, setIsSticky] = useState(false);
+  const NAVBAR_HEIGHT = 66
+
+  const handleScroll = () => {
+    if (headerRef.current) {
+      const rect = headerRef.current.getBoundingClientRect();
+      console.log(rect.top);
+      setIsSticky(rect.top <= NAVBAR_HEIGHT);
+    }
+  };
+
+  useEffect(() => {
+    // Listen for scroll events
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className={`rounded-lg shadow-custom-sm p-4`}>
       <div
+        ref={headerRef}
         onClick={() => {
           if (isFoldable) setUnfold(!unfold);
         }}
@@ -37,7 +59,9 @@ const Box = ({
           isPhone ? "flex-col" : "items-center"
         } ${
           isFoldable ? "hover:text-purple" : ""
-        } cursor-pointer ${getConditionalSmoothTransition(disableAnimation)}`}
+        } cursor-pointer ${getConditionalSmoothTransition(disableAnimation)} ${
+          isSticky && unfold ? "sticky top-[65px] z-10 bg-black py-4" : ""
+        }`}
       >
         <div className="flex items-center gap-4">
           {isFoldable && (
